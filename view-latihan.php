0c312da4ec87444a54b9e4326a4c8d542d1f377c
firdawalia <firdaawalia77@gmail.com>
2023-11-30 14:40:58 +0700

    menambahkan v_about.php, v_footer.php, v_header.php, v_index.php, view-data-anggota.php, view-data-matakuliah.php, view-form-anggota.php, view-form-matakuliah.php, view-latihan.php

---
+++ view-latihan.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+ <title>Latihan 1</title>
+ </head>
+<body>
+    Halo Yeorobun.. Yuk kita belajar web programming bersama..!!!<br>
+    Nilai 1 = <?= $nilai1; ?><br>
+    Nilai 2 = <?= $nilai2; ?><br>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <br> <?=$nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+</html>
